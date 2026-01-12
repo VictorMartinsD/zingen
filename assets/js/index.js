7ac61e27caf9bfceb4900398e0738955ddbe1921
Victor Martins Dias <victormartinsjob@gmail.com>
2026-01-12 14:34:37 -0300

    feat(web): initial landing page structure
    - add images and svgs
    - setup package.json file

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+/* PROJETO: [zingen]
+  AUTOR: Victor Martins
+  DESCRIÇÃO: Script principal para funcionalidades do site.
+*/
+
+// 1. Variáveis Globais e Seletores de Elementos
+
+// 2. Funções Principais
+
+// 3. Eventos (Clicks, Forms, etc)
